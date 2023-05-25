5d0ca10d00f1da1b580389d8db6005fa57874585
ingadi <axelingadi@gmail.com>
2023-05-25 15:06:59 +0300

    use faker to generate db.json

---
+++ data.js
@@ -0,0 +1,37 @@
+const { faker } = require('@faker-js/faker');
+const fs = require('fs');
+
+function createRandomMerchant() {
+  return {
+    merchantId: faker.string.uuid(),
+    firstName: faker.person.firstName(),
+    middleName: faker.person.middleName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    phoneNumber: faker.phone.number(),
+    idNo: faker.helpers.regexpStyleStringParse(
+      '[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'
+    ),
+    country: faker.location.country(),
+    city: faker.location.city(),
+    region: faker.location.state(),
+    category: faker.helpers.arrayElement(['Gold', 'Silver', 'Bronze']),
+    postalAddress: faker.location.zipCode(),
+  };
+}
+
+// return {
+//   merchants: ,
+// };
+
+const data = {
+  merchants: faker.helpers.multiple(createRandomMerchant, { count: 50 }),
+};
+
+fs.writeFile('db.json', JSON.stringify(data), (err) => {
+  if (err) {
+    console.error('Error creating db.json file:', err);
+  } else {
+    console.log('db.json file created successfully.');
+  }
+});
