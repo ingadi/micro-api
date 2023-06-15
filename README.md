cc8edb017853b9a9f0694daf68da2f94213ad8cd
ingadi <axelingadi@gmail.com>
2023-06-15 22:37:58 +0300

    add resource

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ JSON-DB server deployed on cyclic.sh
 - https://chrisdevcode.hashnode.dev/how-to-create-and-deploy-a-json-server
 - https://jasonwatmore.com/fetch-add-bearer-token-authorization-header-to-http-request
 - https://medium.com/codait/environment-variables-or-keeping-your-secrets-secret-in-a-node-js-app-99019dfff716
+- https://github.com/motdotla/dotenv/issues/89
--- index.mjs
+++ index.mjs
@@ -89,3 +89,4 @@ function isAuthenticated({ identifier, password }) {
 // https://chrisdevcode.hashnode.dev/how-to-create-and-deploy-a-json-server
 // https://jasonwatmore.com/fetch-add-bearer-token-authorization-header-to-http-request
 // https://medium.com/codait/environment-variables-or-keeping-your-secrets-secret-in-a-node-js-app-99019dfff716
+// https://github.com/motdotla/dotenv/issues/89
